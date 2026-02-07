b43932bd8cb600cf5d4ac21b2709b01fc2d9eedf
gabrieudev <joao.gabriel@atoscapital.com.br>
2026-02-07 20:00:37 -0300

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@marcahora/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
